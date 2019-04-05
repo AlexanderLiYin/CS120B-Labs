34657eea8a4954b820411fe92a205a6517c2487d
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-05 11:56:41 -0700

    Need to leave

--- ayin005_lab2_part2/ayin005_lab2_part2/main.c
+++ ayin005_lab2_part2/ayin005_lab2_part2/main.c
@@ -10,41 +10,37 @@
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tmpC = 0x00; // Temporary variable to hold the value of B
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A0
 	unsigned char tmpA1 = 0x00; // Temporary variable to hold the value of A1
-	
-	
-	while(1)
+	unsigned char tmpA2 = 0x00; // Temporary variable to hold the value of A0
+	unsigned char tmpA3 = 0x00; // Temporary variable to hold the value of A1
+while(1)
 	{
 		// 1) Read input
 		tmpA0 = PINA & 0x01;
 		tmpA1 = PINA & 0x02;
+		tmpA2 = PINA & 0x03;
+		tmpA3 = PINA & 0x04;
+		int cntavail = 0;
 		// 2) Perform computation
 		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if ((tmpA0 == 0x00) && (tmpA1 == 0x00)) { 
-			tmpC = (tmpC & 0xFC) | 0x04; // Sets tmpB to bbbbbb01
+		if (tmpA0 == 0x01) { // True if PA0 is 1 and PA1 is 0
+			cntavail = cntavail + 1;
 		}
 		
-		else if ((tmpA0 == 0x00) && (tmpA1 == 0x01)) {
-			tmpC = (tmpC & 0xFC) | 0x03;
+		if (tmpA1 == 0x02) { // True if PA0 is 1 and PA1 is 0
+			cntavail = cntavail + 1;
 		}
 		
-		else if ((tmpA0 == 0x01) && (tmpA1 == 0x00)) {
-			tmpC = (tmpC & 0xFC) | 0x02;
+		if (tmpA2 == 0x03) { // True if PA0 is 1 and PA1 is 0
+			cntavail = cntavail + 1;
 		}
 		
-		else if ((tmpA0 == 0x01) && (tmpA1 == 0x01)) {
-			tmpC = (tmpC & 0xFC) | 0x01;
+		if (tmpA3 == 0x04) { // True if PA0 is 1 and PA1 is 0
+			cntavail = cntavail + 1;
 		}
-		
-		else {
-			tmpC = (tmpC & 0xFC) | 0x00;
-		}
-		
 		// 3) Write output
-		PORTC = tmpC; //Assign Port C a number to show how much available 
+		PORTB = cntavail;	
 	}
-	return 0;
 }
