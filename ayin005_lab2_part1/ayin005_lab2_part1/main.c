f3c7c6e2f27e02cf0f6524fb7d0088f53e5f551e
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-05 09:49:03 -0700

    First change

--- ayin005_lab2_part1/ayin005_lab2_part1/main.c
+++ ayin005_lab2_part1/ayin005_lab2_part1/main.c
@@ -7,12 +7,13 @@
 
 #include <avr/io.h>
 
-
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRB = 0xFF; // Configure port B's 8 pins as outputs
+	PORTB = 0x00; // Initialize PORTB output to 0’s
+	while(1)
+	{
+		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+	}
 }
-
+;
