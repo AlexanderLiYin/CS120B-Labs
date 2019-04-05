a92027b728e5c3eeca8624e1f75e17aec1463f7c
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-05 14:12:55 -0700

    Adding part 2 lab 2

---
+++ ayin005_lab2_part3/ayin005_lab2_part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab2_part3.c
+ *
+ * Created: 4/5/2019 2:12:42 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
