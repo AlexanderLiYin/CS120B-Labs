db7993d13e91866cf1cedaef6b67926f7e7cf5ea
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-05 09:48:01 -0700

    Main function

---
+++ ayin005_lab2_part1/ayin005_lab2_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab2_part1.c
+ *
+ * Created: 4/5/2019 9:47:43 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
