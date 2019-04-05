a114f87f568ab4bcb6bf11117408d0e8da82aa62
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-05 10:14:00 -0700

    Lab2 part 2 first attempt

--- ayin005_lab2_part2/ayin005_lab2_part2/main.c
+++ ayin005_lab2_part2/ayin005_lab2_part2/main.c
@@ -11,26 +11,42 @@ int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
 	unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A0
 	unsigned char tmpA1 = 0x00; // Temporary variable to hold the value of A1
+	unsigned char tmpA2 = 0x00; // Temporary variable to hold the value of A2
+	unsigned char tmpA3 = 0x00; // Temporary variable to hold the value of A3
+	unsigned char cntavail = 0x04; //Temp variable used to store number available
+	
+	
 	while(1)
 	{
 		// 1) Read input
 		tmpA0 = PINA & 0x01;
 		tmpA1 = PINA & 0x02;
+		tmpA2 = PINA & 0x03;
+		tmpA3 = PINA & 0x04;
 		// 2) Perform computation
 		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if ((tmpA0 == 0x01) && (tmpA1 == 0x00)) { // True if PA0 is 1 and PA1 is 0
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
-			// (clear rightmost 2 bits, then set to 01)
+		if (tmpA0 == 0x01) { 
+			cntavail++;
 		}
-		else {
-			tmpB = (tmpB & 0xFC) | 0x00; // Sets tmpB to bbbbbb10
-			// (clear rightmost 2 bits, then set to 10)
+
+		if (tmpA1 == 0x01) {
+			cntavail++;
+		}
+		
+		if (tmpA2 == 0x01) {
+			cntavail++;
+		}
+		
+		if (tmpA3 == 0x01) {
+			cntavail++;
 		}
 		// 3) Write output
 		PORTB = tmpB;
+		PORTC = cntavail; //Assign Port C a number to show how much available 
 	}
 	return 0;
 }
