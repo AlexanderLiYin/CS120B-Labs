e2df7957783352805b133480cfc9756a65fef6da
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-08 09:27:09 -0700

    Inserting base code

--- ayin005_lab3_part1/ayin005_lab3_part1/main.c
+++ ayin005_lab3_part1/ayin005_lab3_part1/main.c
@@ -7,12 +7,28 @@
 
 #include <avr/io.h>
 
-
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; // You are UNABLE to read from output pins. Instead you
+	// Should use a temporary variable for all bit manipulation.
+	unsigned char button = 0x00;
+	while(1)
+	{
+		// 1) Read input
+		button = PINA & 0x01;
+		// 2) Perform Computation
+		// if PA0 is 1, set PB1PB0 = 01, else = 10
+		if (button == 0x01) { // True if PA0 is 1
+			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+			// (clear rightmost 2 bits, then set to 01)
+		}
+		else {
+			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
+			// (clear rightmost 2 bits, then set to 10)
+		}
+		// 3) Write Output
+		PORTB = tmpB;	// Sets output on PORTB to value of tmpB
+	}
 }
-
