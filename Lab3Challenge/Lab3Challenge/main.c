c001f3f984a6636e59c2f078049fb5376a4ee764
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-08 14:16:02 -0700

    Lab3 challenge part 1 done

--- Lab3Challenge/Lab3Challenge/main.c
+++ Lab3Challenge/Lab3Challenge/main.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port C's 8 pins as outputs,
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs,
 	// initialize to 0s
 	unsigned char button0 = 0x00;
@@ -19,6 +20,16 @@ int main(void)
 	unsigned char button4 = 0x00;
 	unsigned char button5 = 0x00;
 	unsigned char button6 = 0x00;
+	unsigned char button7 = 0x00;
+	
+	unsigned char output0 = 0x00;
+	unsigned char output1 = 0x00;
+	unsigned char output2 = 0x00;
+	unsigned char output3 = 0x00;
+	unsigned char output4 = 0x00;
+	unsigned char output5 = 0x00;
+	unsigned char output6 = 0x00;
+	unsigned char output7 = 0x00;
 	
 	while(1)
 	{
@@ -30,56 +41,55 @@ int main(void)
 		button4 = PINA & 0x10; // Mask PINA to only get the bit you are interested in
 		button5 = PINA & 0x20; // Mask PINA to only get the bit you are interested in
 		button6 = PINA & 0x40; // Mask PINA to only get the bit you are interested in
+		button7 = PINA & 0x80; // Mask PINA to only get the bit you are interested in
 		
-		int fuel = 0;
-		int light = 0;
+		output0 = PINB & 0x08; // Mask PINA to only get the bit you are interested in
+		output1 = PINB & 0x04; // Mask PINA to only get the bit you are interested in
+		output2 = PINB & 0x02; // Mask PINA to only get the bit you are interested in
+		output3 = PINB & 0x01; // Mask PINA to only get the bit you are interested in
+		output4 = PINC & 0x10; // Mask PINA to only get the bit you are interested in
+		output5 = PINC & 0x20; // Mask PINA to only get the bit you are interested in
+		output6 = PINC & 0x40; // Mask PINA to only get the bit you are interested in
+		output7 = PINC & 0x80; // Mask PINA to only get the bit you are interested in
+		
+		int Bnum = 0;
+		int Cnum = 0;
 		
 		// 2) Perform Computation
 		if (button0 == 0x01) { // True if PA0 is 1
-			fuel = fuel + 1;
+			output4 = 0x10;
+			Cnum = Cnum + 16;
 		}
 		if (button1 == 0x02) { // True if PA1 is 1
-			fuel = fuel + 2;
+			output5 ==0x20;
+			Cnum = Cnum + 32;
 		}
 		if (button2 == 0x04) { // True if PA2 is 1
-			fuel = fuel + 4;
+			output6 == 0x40;
+			Cnum = Cnum + 64;
 		}
 		if (button3 == 0x08) { // True if PA3 is 1
-			fuel = fuel + 8;
+			output7 = 0x80;
+			Cnum = 0x124;
+		}
+		if (button4 == 0x10) { // True if PA0 is 1
+			output3 = 0x01;
+			Bnum = Bnum + 1;
+		}
+		if (button5 == 0x20) { // True if PA1 is 1
+			output2 ==0x02;
+			Bnum = Bnum + 2;
+		}
+		if (button6 == 0x40) { // True if PA2 is 1
+			output1 == 0x04;
+			Bnum = Bnum + 4;
+		}
+		if (button7 == 0x80) { // True if PA3 is 1
+			output0 = 0x08;
+			Bnum = Bnum + 8;
 		}
 		
-		if ((button4 == 0x10) && (button5 == 0x20) && (button6 != 0x40)) { // True if PA3 is 1
-			light = light + 128;
-		}
-		// 3) write results to port
-		if (fuel <= 0)
-		{
-			light = light + 64;
-		}
-		if ((fuel > 0) && (fuel <= 2))
-		{
-			light = light + 96;
-		}
-		if ((fuel >= 3) && (fuel <= 4))
-		{
-			light = light + 112;
-		}
-		if ((fuel >= 5) && (fuel <= 6))
-		{
-			light = light + 56;
-		}
-		if ((fuel >= 7) && (fuel <= 9))
-		{
-			light = light + 60;
-		}
-		if ((fuel >= 10) && (fuel <= 12))
-		{
-			light = light + 62;
-		}
-		if ((fuel >= 13) && (fuel <= 15))
-		{
-			light = light + 63;
-		}
-		PORTC = light;
+		PORTB = Bnum;
+		PORTC = Cnum;
 	}
 }
