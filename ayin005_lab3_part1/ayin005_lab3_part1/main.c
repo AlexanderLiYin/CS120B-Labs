908295810698f94911c45c388152b56cd5f024ed
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-08 09:26:11 -0700

    Lab3

---
+++ ayin005_lab3_part1/ayin005_lab3_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab3_part1.c
+ *
+ * Created: 4/8/2019 9:26:00 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
