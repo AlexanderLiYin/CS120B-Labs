19f10739474a7f6b7ad519028f18529914d6ea0d
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-10 08:37:23 -0700

    Created Lab 4 part 1 file

---
+++ ayin005_lab4_part1/ayin005_lab4_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab4_part1.c
+ *
+ * Created: 4/10/2019 8:32:56 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
