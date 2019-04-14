8e7f8d07ca2da39fa62f55c6fc09701783681cc1
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-14 10:39:11 -0700

    Created lab 4 part 2

---
+++ ayin005_lab4_part2/ayin005_lab4_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab4_part2.c
+ *
+ * Created: 4/14/2019 10:38:51 AM
+ * Author : shado
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
