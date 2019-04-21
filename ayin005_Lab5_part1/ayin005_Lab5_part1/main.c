35ce8463a7ecfca53b5b94ef0bd0d262ce0f69ed
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-21 11:07:11 -0700

    Adding lab 5

---
+++ ayin005_Lab5_part1/ayin005_Lab5_part1/main.c
@@ -0,0 +1,19 @@
+/*
+ * ayin005_Lab5_part1.c
+ *
+ * Created: 4/21/2019 10:34:02 AM
+ * Author : shado
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
