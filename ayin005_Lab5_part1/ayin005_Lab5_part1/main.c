1ebcb14ab56887ad6772bedaa7143b348f68c4f0
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-04-22 09:42:48 -0700

    Updated lab 5

--- ayin005_Lab5_part1/ayin005_Lab5_part1/main.c
+++ ayin005_Lab5_part1/ayin005_Lab5_part1/main.c
@@ -7,13 +7,80 @@
 
 #include <avr/io.h>
 
+
 int main(void)
 {
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs,
+	// initialize to 0s
+	unsigned char button0 = 0x00;
+	unsigned char button1 = 0x00;
+	unsigned char button2 = 0x00;
+	unsigned char button3 = 0x00;
+	unsigned char button4 = 0x00;
+	unsigned char button5 = 0x00;
+	unsigned char button6 = 0x00;
+	
 	while(1)
 	{
-		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+		// 1) Read Inputs and assign to variables
+		button0 = PINA & 0x01; // Mask PINA to only get the bit you are interested in
+		button1 = PINA & 0x02; // Mask PINA to only get the bit you are interested in
+		button2 = PINA & 0x04; // Mask PINA to only get the bit you are interested in
+		button3 = PINA & 0x08; // Mask PINA to only get the bit you are interested in
+		button4 = PINA & 0x10; // Mask PINA to only get the bit you are interested in
+		button5 = PINA & 0x20; // Mask PINA to only get the bit you are interested in
+		button6 = PINA & 0x40; // Mask PINA to only get the bit you are interested in
+		
+		int fuel = 0;
+		int light = 0;
+		
+		// 2) Perform Computation
+		if (button0 == 0x01) { // True if PA0 is 1
+			fuel = fuel + 1;
+		}
+		if (button1 == 0x02) { // True if PA1 is 1
+			fuel = fuel + 2;
+		}
+		if (button2 == 0x04) { // True if PA2 is 1
+			fuel = fuel + 4;
+		}
+		if (button3 == 0x08) { // True if PA3 is 1
+			fuel = fuel + 8;
+		}
+		
+		if ((button4 == 0x10) && (button5 == 0x20) && (button6 != 0x40)) { // True if PA3 is 1
+			light = light + 128;
+		}
+		// 3) write results to port
+		if (fuel <= 0)
+		{
+			light = light + 64;
+		}
+		if ((fuel > 0) && (fuel <= 2))
+		{
+			light = light + 96;
+		}
+		if ((fuel >= 3) && (fuel <= 4))
+		{
+			light = light + 112;
+		}
+		if ((fuel >= 5) && (fuel <= 6))
+		{
+			light = light + 56;
+		}
+		if ((fuel >= 7) && (fuel <= 9))
+		{
+			light = light + 60;
+		}
+		if ((fuel >= 10) && (fuel <= 12))
+		{
+			light = light + 62;
+		}
+		if ((fuel >= 13) && (fuel <= 15))
+		{
+			light = light + 63;
+		}
+		PORTC = light;
 	}
 }
-
-
--- ayin005_lab5_part2/ayin005_lab5_part2/main.c
+++ ayin005_lab5_part2/ayin005_lab5_part2/main.c
@@ -7,27 +7,80 @@
 
 #include <avr/io.h>
 
+// Bit-access function
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) { //x is the button. K is 0 or 1
+	return ((x & (0x01 << k)) != 0);
+}
+
+
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF; // Configure PORTA as input, initialize to 1s
-	DDRB = 0xFF; PORTB = 0x00; // Configure PORTB as outputs, initialize to 0s
-	unsigned char led = 0x00;
-	unsigned char button = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+	// initialize to 0s
+	PORTC = 0x07; //Port C is initialized to 7.
+	enum state {base, increment, decrement};
+	enum state machine;
+	int value=7;
+	int button0;
+	int button1;
+	int check=0;
 	while(1)
 	{
-		// if PA0 is 1, set PB1PB0=01, else =10
-		// 1) Read inputs
-		button = ~PINA & 0x01; // button is connected to A0
-		// 2) Perform Computation
-		if (button) { // True if button is pressed
-			led = (led & 0xFC) | 0x01; // Sets B to bbbbbb01
-			// (clear rightmost 2 bits, then set to 01)
+		int button0 = PINA & 0x01;
+		int button1 = PINA & 0x02;
+		switch(machine)
+		{
+			case base:
+			PORTC = value;
+			
+			if((button0 == 0x01) && (button1 == 0x02))
+			{
+				value = 0;
+				PORTC = value;
+				break;
+			}
+			if ((button0 == 0x01) && (value != 9))
+			{
+				machine = increment;
+				check = 1;
+			};
+			if ((button1 == 0x02) && (value != 0))
+			{
+				machine = decrement;
+				check = 1;
+			}
+			break;
+			
+			case increment:
+			if (check == 1)
+			{
+				value = value + 1;
+				check=0;
+			}
+			
+			PORTC = value;
+			if (button0 != 0x01)
+			{
+				machine = base;
+			}
+			break;
+			
+			case decrement:
+			if (check == 1)
+			{
+				value = value - 1;
+				check=0;
+			}
+			PORTC = value;
+			if (button1 != 0x02)
+			{
+				machine = base;
+			}
+			break;
 		}
-		else {
-			led = (led & 0xFC) | 0x02; // Sets B to bbbbbb10
-			// (clear rightmost 2 bits, then set to 10)
-		}
-		// 3) Write output
-		PORTB = led;
 	}
 }
