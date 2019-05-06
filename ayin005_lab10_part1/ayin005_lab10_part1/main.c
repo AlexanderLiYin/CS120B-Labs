b3df721d5340b3fb2241a6204ab98babd70bc28a
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-05-06 10:04:49 -0700

    Finally got the hardware issues sorted out
    But really behind in terms of lab

---
+++ ayin005_lab10_part1/ayin005_lab10_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_lab10_part1.c
+ *
+ * Created: 5/6/2019 10:04:26 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
