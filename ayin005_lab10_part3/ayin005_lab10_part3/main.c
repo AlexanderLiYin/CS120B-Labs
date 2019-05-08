8e370a6b28dcb9833437478718bba46b3f960860
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-05-08 11:21:01 -0700

    Lab10 part 3 complete

--- ayin005_lab10_part3/ayin005_lab10_part3/main.c
+++ ayin005_lab10_part3/ayin005_lab10_part3/main.c
@@ -74,6 +74,7 @@ unsigned char GetBit(unsigned char x, unsigned char k) { //x is the button. K is
 unsigned char temp=0;
 enum TL_States {TL_SMStart, TL_Seq0, TL_Seq1, TL_Seq2} TL_State;
 enum BL_States {BL_SMStart, BL_LEDOff, BL_LEDOn} BL_State;
+enum SP_States {SP_SMStart, SP_Off, SP_On} SP_State;
 void BlinkLED_Tick()
 {
 	switch (BL_State)
@@ -92,6 +93,28 @@ void BlinkLED_Tick()
 		temp=SetBit(temp, 3, 0);
 		PORTB=temp;
 		BL_State=BL_LEDOn;
+		break;
+	}
+}
+
+void Speaker_Tick()
+{
+	switch (SP_State)
+	{
+		case SP_SMStart:
+		SP_State=SP_On;
+		break;
+		
+		case SP_On:
+		temp=SetBit(temp, 4, 1);
+		PORTB=temp;
+		SP_State=SP_Off;
+		break;
+		
+		case SP_Off:
+		temp=SetBit(temp, 4, 0);
+		PORTB=temp;
+		SP_State=SP_On;
 		break;
 	}
 }
@@ -129,17 +152,23 @@ void ThreeLED_Tick()
 
 void main()
 {
+	DDRA = 0x00; // Set port B to output
+	PORTA = 0xFF; // Init port B to 0s
 	DDRB = 0xFF; // Set port B to output
 	PORTB = 0x00; // Init port B to 0s
+	
 	unsigned long BL_elapsedTime = 0;
 	unsigned long TL_elapsedTime = 0;
-	const unsigned long timerPeriod = 100;
+	unsigned long SP_elapsedTime = 0;
+	unsigned char tmpA1;
+	
+	const unsigned long timerPeriod = 2;
 	TimerSet(timerPeriod);
 	TimerOn();
 	
-	
 	BL_State=BL_SMStart;
 	TL_State=TL_SMStart;
+	SP_State=SP_SMStart;
 	while(1) {
 		if (TL_elapsedTime>=300)
 		{
@@ -151,9 +180,16 @@ void main()
 			BlinkLED_Tick();
 			BL_elapsedTime=0;
 		}
+		tmpA1 = PINA & 0x02;
+		if ((tmpA1 != 0x02) && (SP_elapsedTime >= 2))
+		{
+			Speaker_Tick();
+			SP_elapsedTime=0;
+		}
 		while(!TimerFlag);
 		TimerFlag=0;
 		BL_elapsedTime += timerPeriod;
 		TL_elapsedTime += timerPeriod;
+		SP_elapsedTime += timerPeriod;
 	}
 }
