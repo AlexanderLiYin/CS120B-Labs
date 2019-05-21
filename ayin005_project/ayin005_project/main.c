e9938d19c0708e13e984c09bce34ca9a5e8e8d92
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-05-21 09:51:48 -0700

    Created project files

---
+++ ayin005_project/ayin005_project/main.c
@@ -0,0 +1,18 @@
+/*
+ * ayin005_project.c
+ *
+ * Created: 5/21/2019 9:40:37 AM
+ * Author : shado
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
