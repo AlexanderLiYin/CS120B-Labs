d0d68c51a974f9177c12fc303988ba7e3ab9917e
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2019-05-21 11:02:15 -0700

    Added a bunch of include files with various functions

--- ayin005_project/ayin005_project/main.c
+++ ayin005_project/ayin005_project/main.c
@@ -6,13 +6,20 @@
  */ 
 
 #include <avr/io.h>
-
+#include "timer.h" //used for timer
+#include "io.h" //used for LCD
+#include "stack.h" //stack functions
+//#include "seven_seg.h" //code for seven segment display
+#include "queue.h" //code for a queue
+#include "bit.h" //code for set and get bit
+#include "keypad.h" //code for the keypad
 
 int main(void)
 {
     /* Replace with your application code */
     while (1) 
     {
+		
     }
 }
 
